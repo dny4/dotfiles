44e2053e9037a36b8bbb43381c85843dc8ead2bd
dny4 <dm.raut49@gmail.com>
2023-10-31 18:33:32 +0530

    tmux scripts

---
+++ .local/scripts/remove-session.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+session_name=$(
+	tmux list-sessions -F \{#S\} |
+		tr -d "{}" |
+		fzf \
+			--height=55 --layout=reverse --border=sharp --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#ff7b72 \
+			--color=fg:#cdd6f4,header:#ff7b72,info:#cba6f7,pointer:#f5e0dc \
+			--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#ff7b72 \
+			--color=border:#b4befe
+)
+
+tmux kill-session -t $session_name 2>/dev/null
